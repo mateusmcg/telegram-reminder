0d7972492b44415b706ea7aaef4ed3de1ecebf62
Mateus C <mateus_mcg@icloud.com>
2016-08-22 19:39:17 -0300

    Fix start script

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
