1f33529d56469578961e0d716af7f586a9861caa
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-23 19:20:21 -0300

    Fix start command regex

--- bot/pill-reminder-bot.js
+++ bot/pill-reminder-bot.js
@@ -6,7 +6,7 @@ var bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.setWebHook(process.env.HEROKU_URL + bot.token);
 
-bot.onText(/^\/start (.+)$/, function (msg, match) {
+bot.onText(/^\/start/, function (msg, match) {
     var chatId = msg.chat.id;
     bot.sendMessage(msg.chat.id, 'Agora você receberá notificações às 00:30. \nCaso queira cancelar o serviço, utilize o comando /cancel !').then(function () {
         // reply sent!
