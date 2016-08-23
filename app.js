f2d614b57698171ab524820b75b36533dffb35dc
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-23 01:35:15 -0300

    Fix commands

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 5000));
 app.set('TELEGRAM_API_URL', process.env.TELEGRAM_BOT_TOKEN);
 app.set('TELEGRAM_BOT_TOKEN', process.env.TELEGRAM_BOT_TOKEN || '263464526:AAGLXjdte-AwkImK0s4n_zqQwiaSCVDePeI');
 
-mongoose.connect('mongodb://mateusc:mateusc@ds013916.mlab.com:13916/heroku_wlxnk213', function (error) {
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
     if (error) console.error(error);
     else console.log('mongo connected');
 });
@@ -21,32 +21,34 @@ var bot = new TelegramBot(app.get('TELEGRAM_BOT_TOKEN'), { polling: true });
 
 // Any kind of message
 bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
+    var chatId = msg.chat.id.toString();
     var message = msg.text;
 
     switch (message) {
         case '/start': {
             try {
-                var ReminderTelegram = mongoose.model('ReminderTelegram', { chatId: String });
-                var obj = new ReminderTelegram({
-                    chatId: chatId.toString()
-                });
-                obj.save(function (err) {
-                    bot.sendMessage(chatId, 'Agora você receberá notificações às 00:30. \nCaso queira cancelar o serviço, utilize o comando /cancel');
-                });
+                var Model = mongoose.model('ReminderTelegram', { chatId: String });
+                var obj = new Model({ chatId: chatId });
+                Model.findOne({ chatId: chatId }, function (err, doc) {
+                    if (doc) {
+                        bot.sendMessage(chatId, 'Você já está com o serviço de lembrete ligado. Caso queira desativar utilize o comando /cancel');
+                    } else {
+                        obj.save(function (err) {
+                            bot.sendMessage(chatId, 'Agora você receberá notificações às 00:30. \nCaso queira cancelar o serviço, utilize o comando /cancel');
+                        });
+                    }
+                })
             } catch (e) {
                 bot.sendMessage(chatId, e.name + '/' + e.message);
             }
         } break;
         case '/cancel': {
             try {
-                var ReminderTelegram = mongoose.model('ReminderTelegram', { chatId: String });
-                var obj = new ReminderTelegram({chatId: chatId.toString()});
-                obj.remove();
-                obj.save(function(err){
+                var Model = mongoose.model('ReminderTelegram', { chatId: String });
+                Model.find().remove({ chatId: chatId.toString() }, function (err) {
                     bot.sendMessage(chatId, 'Serviço cancelado com sucesso!');
-                });
-            } catch (e){
+                })
+            } catch (e) {
                 bot.sendMessage(chatId, e.name + '/' + e.message);
             }
         } break;
