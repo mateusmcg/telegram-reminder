9119568ffa79ff8feb4f9d4d27e772bc0652864f
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-09-06 19:47:05 -0300

    Fixed url of new cron job - sgv-trigger

--- bin/sgv-trigger.sh
+++ bin/sgv-trigger.sh
@@ -2,4 +2,6 @@
 
 date
 
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api-sgv.azurewebsites.net/
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api-sgv-release.azurewebsites.net/
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET https://api-sgv-develop.azurewebsites.net/
